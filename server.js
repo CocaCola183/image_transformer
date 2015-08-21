c0fcf175cdaa64cdf179fac8057a92d07933f985
Kivi <Kivi>
2015-08-21 11:32:44 +0800

    add by kivi
    add crop

--- server.js
+++ server.js
@@ -48,6 +48,29 @@ server.get('/images/:width/:height/:url', function(req, res, next) {
 	});
 });
 
+server.get('/images/crop/:width/:height/:x/:y/:url', function(req, res, next) {
+	console.log('*********************************************************************************************');
+	console.log('request_url:', req.url);
+	console.log('operation: crop image');
+	console.log('image_width:', req.params.width);
+	console.log('image_height:', req.params.height);
+	console.log('image_x:', req.params.x);
+	console.log('image_y:', req.params.y);
+	console.log('image_url:', req.params.url);
+	console.log('*********************************************************************************************');
+	var wtireStream = gm(req.params.url).crop(req.params.width, req.params.height, req.params.x, req.params.y).stream();
+	wtireStream.pipe(res);
+	wtireStream.on('end', function() { next(false); });
+	gm(req.params.url).format(function(err, format) {
+		if(err) {
+			console.log('Get image type failed:', err);
+		} else {
+			wtireStream.pipe(fs.createWriteStream('./croped-server.' + format.toLowerCase()));
+		}
+	});
+});
+
+
 server.listen(8081, function() {
   console.log('%s listening at %s', server.name, server.url);
 });
